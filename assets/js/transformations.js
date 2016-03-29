087b5ad9eac08bfbf21494bd7f8ab2cadd27a2d6
mkoleva <mskoleva@live.com>
2016-03-29 20:32:38 +0200

    Animations to all using classes to-animate and in-view, simplification of jquery trigger.

--- assets/js/transformations.js
+++ assets/js/transformations.js
@@ -2,7 +2,6 @@
  * Created by mim on 28.03.16.
  */
 
-
 var $window = $(window);
 
 function isElementInViewport(el) {
@@ -15,78 +14,18 @@ function isElementInViewport(el) {
 }
 
 function check_if_in_view() {
-    console.log('triggered stuff');
+    //If an element comes in view, add new class. This will animate the transition
+
     var $animation_elements = $('.to-animate');
-    console.log($animation_elements.length);
 
     $.each($animation_elements, function() {
         var $element = $(this);
-        console.log('checking elements');
         if (isElementInViewport($element))
          {
-             $(this).hide().delay(1000).fadeIn("slow");
-             //$element.find('img').delay(2000).animate({opacity:1}, slow);
              $element.addClass('in-view', 2000);
-             $element.removeClass('to-animate');
-             console.log('added in view class')
         }
     });
 }
 
 $window.on('load scroll resize', check_if_in_view);
 $window.trigger('scroll');
-
-function animations() {
-    $(this).hide().delay(100).fadeIn("slow");
-    //$('#home').find('img').delay(1400).animate({
-    //    opacity: 1,
-    //        left: 0
-    //    }, 'slow'
-    //);
-
-}
-
-//
-//$('.blogs').find('p').delay(1400).animate({
-//        opacity: 1,
-//        left: 0
-//    }, 'slow'
-//);
-//
-//$('.blogs').find('img').delay(2000).animate({
-//        opacity: 1,
-//        right: 0
-//    }, 'slow'
-//);
-//
-//$('.blogs').find('button').delay(2500).animate({
-//        opacity: 1,
-//        bottom: 0
-//    }, 'slow'
-//);
-
-
-//function onVisibilityChange(el, callback) {
-//    var old_visible;
-//    return function () {
-//        var visible = isElementInViewport(el);
-//        if (visible != old_visible) {
-//            old_visible = visible;
-//            if (typeof callback == 'function') {
-//                console.log("callback called")
-//                callback();
-//            }
-//        }
-//    }
-//}
-//
-//var handler = onVisibilityChange(null, function() {
-//    /* your code go here */
-//    $element.addClass('in-view');
-//    console.log("triggered a change")
-//});
-//
-//
-////jQuery
-//$(window).on('DOMContentLoaded load resize scroll', handler);
-//$(window).trigger('scroll');
